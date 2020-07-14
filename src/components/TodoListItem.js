9a346d9f7f1d0e323bbac34ae0346325591d499a
artmadiar <artmadiar@gmail.com>
2020-07-14 15:22:26 +0200

    select important tasks

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const Item = ({ id, label, important = false }) => {
-  console.log({id, label, important });
-  const liStyle = {
-    color: important ? 'tomato' : 'black',
-  };
+const Item = ({ id, label, important = true }) => {
+  const importantClass = important ? 'important' : '';
 
-  return <li className="list-group-item todo-list-item" key={id} >
-    <span style={liStyle} className="todo-list-item-label" >{ label }</span>
-    <span>
+  return <li className={`list-group-item todo-list-item ${importantClass}`} key={id} >
+    <span className="todo-list-item-label" >{ label }</span>
+    <span className="todo-list-item-buttons">
       <button type="button" className="btn btn-outline-success btn-sm">
         <i className="fa fa-exclamation" />
       </button>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import SearchPanel from './components/SearchPanel';
 const list = [{
   id: 1,
   label: 'learn react',
+  important: false,
 }, {
   id: 2,
   label: 'learn flexbox',
